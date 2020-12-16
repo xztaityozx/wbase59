ce2da73814b4bcd92fad5a3b884e3539f2a855de
xztaityozx <taityonohibi@gmail.com>
2020-12-16 23:46:42 +0900

    Ivsレコードとそのテスト

--- test/IvsTest.cs
+++ test/IvsTest.cs
@@ -1,12 +1,30 @@
 using System;
 using Xunit;
-using wbase56;
+using wbase59;
 
 namespace test {
     public class IvsTest {
         [Fact]
         public void Test_IsEmpty() {
-            
+            var data = new[] {
+                new {ivs = new Ivs(null), expect = true},
+                new {ivs = new Ivs(1), expect = false},
+                new {ivs = new Ivs(128), expect = false},
+                new {ivs = new Ivs(0), expect = false}
+            };
+
+            foreach (var item in data) {
+                Assert.Equal(item.expect, item.ivs.IsEmpty);
+            }
+        }
+
+        [Fact]
+        public void Test_GetSpan() {
+            for (var b = 0; b <= byte.MaxValue; b++) {
+                Assert.Equal(new byte[] {0xdb, 0x40, 0xdd, (byte) b}, new Ivs((byte) b).GetSpan().ToArray());
+            }
+
+            Assert.Throws<InvalidOperationException>(() => new Ivs(null).GetSpan());
         }
     }
 }
---
+++ wbase59/Nabe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace wbase59 {
+    public enum BaseNabe {
+        辺,邉,邊
+    }
+
+    /// <summary>
+    /// IVSを表すレコード
+    /// </summary>
+    public record Ivs(byte? Position) {
+        /// <summary>
+        /// IVSがあるかどうか
+        /// </summary>
+        public bool IsEmpty => Position is null;
+        private byte[] bytes;
+
+        /// <summary>
+        /// IVSのバイト列をSpanで返す
+        /// </summary>
+        /// <exception cref="InvalidOperationException">IVSがないときはIVSのバイト列を生成出来ない</exception>
+        /// <returns>IVSのバイト列</returns>
+        public Span<byte> GetSpan() {
+            if(Position is null) throw new InvalidOperationException();
+            
+            bytes ??= new byte[] {0xdb, 0x40, 0xdd, (byte)Position};
+            return new Span<byte>(bytes);
+        }
+    }
+
+}
