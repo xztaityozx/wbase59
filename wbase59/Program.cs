2a3635ff9ec5a71d88122bb3cee61fdfe782503f
xztaityozx <taityonohibi@gmail.com>
2020-12-16 21:59:51 +0900

    ひな形作った

---
+++ wbase59/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace wbase59 {
+    internal class Program {
+        private static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
