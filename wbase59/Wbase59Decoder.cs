4ec772aa7e6c879e1e8629f4d57f89a816ca0976
xztaityozx <taityonohibi@gmail.com>
2020-12-26 12:46:51 +0900

    入力ストリームのエンコードを指定できたりするようにした

--- wbase59/Wbase59Decoder.cs
+++ wbase59/Wbase59Decoder.cs
@@ -10,18 +10,22 @@ namespace Wbase59 {
     public class Wbase59Decoder {
         private readonly StreamReader stream;
 
-        public Wbase59Decoder(Stream stream) => this.stream = new StreamReader(stream);
+        public Wbase59Decoder(Stream stream, Encoding encoding)
+            => this.stream = new StreamReader(stream, encoding);
 
+        /// <summary>
+        /// デコード本体
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<byte> Decode() {
-            var encoder = new UnicodeEncoding(true, false);
-
             var nextMod = 0;
             var stack = new Stack<int>();
+            var encoding = new UnicodeEncoding(true, false);
 
             while (stream.Peek() > 0) {
                 for (var item = StringInfo.GetTextElementEnumerator(stream.ReadLine() ?? string.Empty);
                     item.MoveNext();) {
-                    var bytes = encoder.GetBytes(item.GetTextElement());
+                    var bytes = encoding.GetBytes(item.GetTextElement());
 
                     var nextNabe = Nabe.Parse(bytes.AsSpan());
 
@@ -33,7 +37,7 @@ namespace Wbase59 {
                         nextMod = (int) nextNabe.Base;
                     }
                     else {
-                        stack.Push(Wbase59.ToByteValue(nextNabe));
+                        stack.Push(Wbase59.ToValue(nextNabe));
                     }
                 }
 
@@ -42,6 +46,13 @@ namespace Wbase59 {
             }
         }
 
+        /// <summary>
+        /// ナベビットから復元した余りと商を使ってもとの値を復元する君
+        /// </summary>
+        /// <param name="stack"></param>
+        /// <param name="mod"></param>
+        /// <returns></returns>
+        /// <exception cref="ParityNabeCheckFailedException">パリティチェックに失敗したときに投げられる</exception>
         private static int Aggregate(Stack<int> stack, int mod) {
             var cur = 0;
 
--- wbase59/Wbase59Encoder.cs
+++ wbase59/Wbase59Encoder.cs
@@ -7,21 +7,20 @@ using System.Text;
 namespace Wbase59 {
     public class Wbase59Encoder {
         private readonly StreamReader stream;
-        public Wbase59Encoder(Stream stream) => this.stream = new StreamReader(stream);
+        public Wbase59Encoder(Stream stream, Encoding encoding) => this.stream = new StreamReader(stream, encoding);
 
         public IEnumerable<Nabe> Encode() {
-            var encoder = new UnicodeEncoding(true, false);
             while (stream.Peek() > 0) {
                 foreach (var b in Encoding.UTF8.GetBytes(stream.ReadLine() ?? string.Empty)) {
                     var val = b;
                     yield return new Nabe((BaseNabe) (val % 3));
 
                     do {
-                        yield return Wbase59.Create(val % Max);
+                        yield return Wbase59.ToNabe(val % Max);
                         val /= Max;
                     } while (val >= Max);
 
-                    yield return Wbase59.Create(val);
+                    yield return Wbase59.ToNabe(val);
                 }
             }
         }
